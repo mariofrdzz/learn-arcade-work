fe6dfa58eb0f81249bd5326c541266774ff9f60a
Mario Fernandez <mario.fernandezgarci@edu.uah.es>
2025-02-24 11:08:40 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-testing
+print("This is my first program.")
+print("Let's just take it easy and start learning!")
+print("I hope the next ones will be\nmuch more complex.")
+print("Let's add some formatting:\tPython is fun!") 
