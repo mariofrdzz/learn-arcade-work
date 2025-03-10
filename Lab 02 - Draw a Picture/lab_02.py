b187b4871dd568468c91cce8836e84cfd6f9306c
Mario Fernandez <mario.fernandezgarci@edu.uah.es>
2025-03-10 11:39:17 +0100

    Lab 02 done

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -2,32 +2,57 @@
 import arcade
 
 # Open up a window.
-arcade.open_window(800, 600, "Villa oculta de la hoja")
+arcade.open_window(800, 600, "Coche en la carretera")
 
 # Color de fondo
 gris_claro = (194,194,194)
-arcade.set_background_color(gris_claro)
+arcade.set_background_color(arcade.color.SKY_BLUE)
 
 # Listo para dibujar
 arcade.start_render()
 
-# Comenzamos con la hoja
+# Carretera con linea
+arcade.draw_lrbt_rectangle_filled(0,800,0,200,arcade.color.GRAY)
+for i in range(50, 800, 100):
+    arcade.draw_lrbt_rectangle_filled(i,i+40,95,105,arcade.color.YELLOW)
 
-# Dibujar la espiral
-arcade.draw_arc_outline(300, 300, 200, 200, arcade.color.BLACK, 0, 270, 10)
-arcade.draw_arc_outline(300, 300, 150, 150, arcade.color.BLACK, 0, 270, 10)
-arcade.draw_arc_outline(300, 300, 100, 100, arcade.color.BLACK, 0, 270, 10)
-arcade.draw_arc_outline(300, 300, 50, 50, arcade.color.BLACK, 0, 270, 10)
+# Carroceria del coche
+arcade.draw_lrbt_rectangle_filled(250,380,130,250,arcade.color.RED)
+arcade.draw_lrbt_rectangle_filled(210,470,130,190,arcade.color.RED)
+arcade.draw_triangle_filled(250,250,220,190,250,190,arcade.color.RED)
+arcade.draw_triangle_filled(380,250,440,190,380,190,arcade.color.RED)
 
-# Dibujar el triángulo inferior
-arcade.draw_triangle_filled(180, 200, 280, 200, 230, 120, arcade.color.BLACK)
+# Ventanas
+arcade.draw_triangle_filled(250,245,225,190,250,190,arcade.color.LIGHT_BLUE)
+arcade.draw_triangle_filled(380,245,435,190,380,190,arcade.color.LIGHT_BLUE)
+arcade.draw_lrbt_rectangle_filled(340,380,190,245,arcade.color.LIGHT_BLUE)
+arcade.draw_lrbt_rectangle_filled(255,335,190,245,arcade.color.LIGHT_BLUE)
 
-# Dibujar las líneas inclinadas
-arcade.draw_line(400, 400, 460, 460, arcade.color.BLACK, 10)
-arcade.draw_line(190, 290, 250, 350, arcade.color.BLACK, 10)
+# Ruedas
+arcade.draw_circle_filled(250,130,25,arcade.color.BLACK)
+arcade.draw_circle_filled(430,130,25,arcade.color.BLACK)
 
-# Dibujar el círculo exterior
-arcade.draw_circle_outline(300, 300, 220, arcade.color.BLACK, 10)
+
+# Palmera 1
+# Tronco de la palmera
+arcade.draw_lrbt_rectangle_filled(185, 215, 200, 450, arcade.color.BROWN)
+
+# Hojas de la palmera (usamos varias elipses para las hojas)
+arcade.draw_ellipse_filled(200 - 40, 400 + 30, 80, 30, arcade.color.ARCADE_GREEN)
+arcade.draw_ellipse_filled(200 + 40, 400 + 30, 80, 30, arcade.color.BUD_GREEN)
+arcade.draw_ellipse_filled(200 - 30, 400 + 50, 70, 30, arcade.color.ANDROID_GREEN)
+arcade.draw_ellipse_filled(200 + 30, 400 + 50, 70, 30, arcade.color.DARK_GREEN)
+
+
+# Palmera 2
+# Tronco de la palmera
+arcade.draw_lrbt_rectangle_filled(585, 615, 200, 450, arcade.color.BROWN)
+
+# Hojas de la palmera (usamos varias elipses para las hojas)
+arcade.draw_ellipse_filled(600 - 40, 400 + 30, 80, 30, arcade.color.ARCADE_GREEN)
+arcade.draw_ellipse_filled(600 + 40, 400 + 30, 80, 30, arcade.color.BUD_GREEN)
+arcade.draw_ellipse_filled(600 - 30, 400 + 50, 70, 30, arcade.color.ANDROID_GREEN)
+arcade.draw_ellipse_filled(600 + 30, 400 + 50, 70, 30, arcade.color.DARK_GREEN)
 
 # --- Finish drawing ---
 arcade.finish_render()
--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,18 @@
-print ("Hello world")
+import arcade
+
+# Abrir una ventana
+arcade.open_window(600, 600, "Ventana de prueba")
+
+# Establecer el color de fondo
+arcade.set_background_color(arcade.color.WHITE)
+
+# Función que se llama para dibujar en la pantalla
+def on_draw():
+    arcade.start_render()
+    arcade.draw_text("¡Hola, Arcade!", 200, 300, arcade.color.BLACK, 24)
+
+# Asignar la función `on_draw` al evento de dibujo
+arcade.set_draw_function(on_draw)
+
+# Ejecutar el juego
+arcade.run()
